c208b43f18e8aa34de4bd13257928b31397156e8
vinicioCoelho <coelhocvcg@gmail.com>
2023-10-19 19:34:29 -0300

    criando inputs e variáveis

---
+++ CEV/aula4.py
@@ -0,0 +1,17 @@
+print('texto')
+print('7 + 4')
+print('7'+'4')
+print('Olá', 5)
+
+# toda variável é um objeto
+
+nome = 'texto'
+idade = '25'
+peso = '75.8'
+print(nome, idade, peso)
+
+# interatividade
+nome = input('Qual seu nome?')
+idade = input('Qual sua idade?')
+peso = input('Qual é o seu peso?')
+print(nome,idade,peso)
