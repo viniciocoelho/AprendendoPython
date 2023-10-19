45e80502c4111c718df930a8138e6d7f5b42922a
vinicioCoelho <coelhocvcg@gmail.com>
2023-10-19 20:24:21 -0300

    usando a função format

--- CEV/aula4.py
+++ CEV/aula4.py
@@ -1,17 +1,27 @@
-print('texto')
-print('7 + 4')
-print('7'+'4')
-print('Olá', 5)
+# print('texto')
+# print('7 + 4')
+# print('7'+'4')
+# print('Olá', 5)
 
-# toda variável é um objeto
+# # toda variável é um objeto
 
-nome = 'texto'
-idade = '25'
-peso = '75.8'
-print(nome, idade, peso)
+# nome = 'texto'
+# idade = '25'
+# peso = '75.8'
+# print(nome, idade, peso)
 
-# interatividade
-nome = input('Qual seu nome?')
-idade = input('Qual sua idade?')
-peso = input('Qual é o seu peso?')
-print(nome,idade,peso)
+# # interatividade
+# nome = input('Qual seu nome?')
+# idade = input('Qual sua idade?')
+# peso = input('Qual é o seu peso?')
+# print(nome,idade,peso)
+
+# nome = input('Qual é o seu nome?')
+# print('Olá',nome,'Prazer em te conhecer')
+
+n1 = int(input('nr 1:'))
+n2 = int(input('nr 2:'))
+s = n1 + n2
+print('A soma entre {} e {} vale {}'.format(n1,n2,s))
+
+n = float(input('Digite um valor:'))
